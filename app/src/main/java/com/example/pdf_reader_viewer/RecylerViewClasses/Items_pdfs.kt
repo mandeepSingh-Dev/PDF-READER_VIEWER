e66897f6fe4ed4f0d5ae5d9bd2b6e35e02aea1cc
Mandeep Singh <mandeepsinghsaggu2@gmail.com>
2022-01-08 19:24:41 +0530

    data/model  class for pdf list_items

--- app/src/main/java/com/example/pdf_reader_viewer/RecylerViewClasses/Items_pdfs.kt
+++ app/src/main/java/com/example/pdf_reader_viewer/RecylerViewClasses/Items_pdfs.kt
@@ -2,4 +2,7 @@ package com.example.pdf_reader_viewer.RecylerViewClasses
 
 import android.net.Uri
 
-data class Items_pdfs(var displayName:String, var size:String, var uri: Uri?,var date_modified:String?)
+data class Items_pdfs(var title:String, var size:String, var appendeduri: Uri?,var date_modified:String?,var relativePath:String,var bucket:String?)
+{
+    constructor(title:String,size:String,appendeduri:Uri?,date_modified:String?,bucket:String?):this( title,  size,  appendeduri, date_modified,"Not available",bucket)
+}
