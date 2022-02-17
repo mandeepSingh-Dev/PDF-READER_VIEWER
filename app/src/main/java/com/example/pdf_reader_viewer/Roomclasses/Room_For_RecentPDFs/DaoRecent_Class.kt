e7a0eaaff6385efbbe5a730276bbf0207818c436
Mandeep Singh <mandeepsinghsaggu2@gmail.com>
2022-02-17 17:43:04 +0530

    Added new query method for getting item by pdfuri

--- app/src/main/java/com/example/pdf_reader_viewer/Roomclasses/Room_For_RecentPDFs/DaoRecent_Class.kt
+++ app/src/main/java/com/example/pdf_reader_viewer/Roomclasses/Room_For_RecentPDFs/DaoRecent_Class.kt
@@ -2,6 +2,7 @@ package com.example.pdf_reader_viewer.Roomclasses.Room_For_RecentPDFs
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
+import com.example.pdf_reader_viewer.Roomclasses.Room_For_BOOKMARKS.Items_Bookmarks
 
 @Dao
 interface DaoRecent_Class{
@@ -20,4 +21,7 @@ interface DaoRecent_Class{
 
      @Query("SELECT * FROM Recent_PDF_Table WHERE item_id Like :id")
      fun queryByID(id:Int): Items_RecentPdfs
+
+    @Query("SELECT * FROM Recent_PDF_Table WHERE pdfUri Like :pdfUri")
+    fun query(pdfUri: String): Items_RecentPdfs
 }
